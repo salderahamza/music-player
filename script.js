f33a33d96502625fe771a81ec4ffb5363c3f00d5
dev-zarl <hamzasafdark@gmail.com>
2020-09-05 15:37:42 +0500

    play/pause and song-switching included

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
 
+// Music
+const song = [
+  {
+    name: "music-1",
+    displayName: "Raggae Fusion",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "music-2",
+    displayName: "Seven Nation Army",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "music-3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "metric-1",
+    displayName: "Front Row",
+    artist: "Jacinto Design",
+  },
+];
 // Check if playing
 let isPlaying = false;
 
@@ -20,5 +46,47 @@ function pauseSong() {
   music.pause();
 }
 
+function prevSong() {
+  if (songIndex != 0) {
+    songIndex--;
+    loadSong(song[songIndex]);
+    playSong();
+  } else {
+    songIndex = 3;
+    loadSong(song[songIndex]);
+    playSong();
+  }
+}
+
+function nextSong() {
+  if (songIndex != 3) {
+    songIndex++;
+    loadSong(song[songIndex]);
+    playSong();
+  } else {
+    songIndex = 0;
+    loadSong(song[songIndex]);
+    playSong();
+  }
+}
+
 // Play or Pause
 playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// Add songs to DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// On Load - Select First Song
+loadSong(song[songIndex]);
+
+//Event Listeners
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
